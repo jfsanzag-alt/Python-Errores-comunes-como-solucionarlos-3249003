64ed9b05765f54263716406be48b045525a5963a
jfsanzag-alt <jfsanzag@gmail.com>
2026-02-05 19:18:04 +0000

    JFSA Cambios para aprender

--- c4_tiempo_ejecucion/c4v07_archivo_inexistente.py
+++ c4_tiempo_ejecucion/c4v07_archivo_inexistente.py
@@ -1,7 +1,26 @@
 import csv
 
-with open("archivos/datos.csv", "r", encoding="UTF-8") as archivo:
+#with open("/workspaces/Python-Errores-comunes-como-solucionarlos-3249003/c4_tiempo_ejecucion/archivos/datos.csv", "r", encoding="UTF-8") as archivo:
+with open("c4_tiempo_ejecucion/archivos/datos.csv", "r", encoding="UTF-8") as archivo:
     reader = csv.reader(archivo)
     columnas = next(reader)
     for fila in reader:
         print(fila[0])
+
+import os
+
+ruta_actual = os.getcwd()
+print(f"Estás trabajando en: {ruta_actual}")
+
+# Ruta completa al archivo
+ruta_archivo = os.path.abspath(__file__)
+
+# Solo la carpeta que contiene al archivo
+directorio_script = os.path.dirname(ruta_archivo)
+
+print(f"El archivo está en: {ruta_archivo}")
+print(f"La carpeta del script es: {directorio_script}")
+
+import sys
+
+print(f"El ejecutable de Python es: {sys.executable}")
